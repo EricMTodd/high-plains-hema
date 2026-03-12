b6d235a438d80aa2bec125a721737f3ccc117b0a
EricMTodd <eterictodd@gmail.com>
2026-03-12 15:30:31 -0600

    Basic page layout achieved.

--- high_plains_hema/views.py
+++ high_plains_hema/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """The home page for High Plains HEMA."""
+    return render(request, 'high_plains_hema/index.html')
